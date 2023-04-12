e14a18bec11d5145bd16117b6512faaf573184f9
Christian Medina <117231596+cjmedina95@users.noreply.github.com>
2023-04-12 16:29:03 -0500

    Create mostFrequentChar.js

---
+++ array and string/most frequent char/mostFrequentChar.js
@@ -0,0 +1,25 @@
+const mostFrequentChar = (s) => {
+  let max_length = 0;
+  let letter = null;
+
+  let count = {};
+  
+  for (let char of s) {
+    if (!count[char]) {
+      count[char] = 0;
+    }
+    count[char] += 1;
+  }
+  
+  for (let key in count) {
+    if (!letter || count[key] > count[letter]) {
+      letter = key;
+    }
+  }
+  
+  return letter;
+};
+
+module.exports = {
+  mostFrequentChar,
+};
