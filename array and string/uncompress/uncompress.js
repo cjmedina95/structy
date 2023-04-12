6cfca243ede849ba68a420b5e6f15ed4161ef132
Christian Medina <117231596+cjmedina95@users.noreply.github.com>
2023-04-12 14:49:38 -0500

    Create uncompress.js

---
+++ array and string/uncompress/uncompress.js
@@ -0,0 +1,30 @@
+const uncompress = (s) => {
+  let uncompressed_string = [];
+  const numbers = '0123456789'
+  
+  let letterPointer = 0;
+  let numericPointer = 0;
+  
+  while (letterPointer < s.length) {
+    
+    if (numbers.includes(s[letterPointer])) {
+        letterPointer += 1;
+    }
+    else {
+      const numberStore = Number(s.slice(numericPointer, letterPointer));
+      
+      for (let count = 0; count < numberStore; count += 1) {
+        uncompressed_string.push(s[letterPointer]);
+      }      
+      
+      letterPointer += 1;
+      numericPointer = letterPointer;
+    }
+  }
+    
+  return uncompressed_string.join('');
+};
+
+module.exports = {
+  uncompress,
+};
